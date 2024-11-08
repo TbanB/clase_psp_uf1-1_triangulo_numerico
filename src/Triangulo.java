608b6f63755344866f0910130938085c0f3619b5
Esteban Bennett <estebanBennettb@gmail.com>
2024-11-08 22:41:53 +0100

    comentarios añadidos

--- src/Triangulo.java
+++ src/Triangulo.java
@@ -1,21 +1,57 @@
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Definición de la clase Triangulo.
+ */
 public class Triangulo {
+
+    /**
+     * Declaramos el método main de la clase Triangulo.
+     *
+     * Un apunte personal:
+     *      * main debe ser publico para que sea accesible desde cualquier parte del programa,
+     *      si no fuese publico la JVM no podría acceder a ella y así ejecutar el programa.
+     *      * static identifica que el método pertenece a la clase y no es una instancia especifica de la clase.
+     *      Un método static se puede llamar sin necesidad de de crear un objeto de clase.
+     *      * Void hace referencia al retorno del método, en este caso void identifica que el método no devuelve ningún valor.
+     * @param args
+     */
     public static void main(String[] args)
     {
+
+        /**
+         * Validamos si entran argumentos al ejecutar el programa,
+         * en este caso deberían ser un 5 7 o 9 dependiendi del proceso ejecutado
+         */
         if (args.length == 0)
         {
             System.out.println("Se requiere un argumento");
             return;
         }
 
+        /**
+         * En este bloque obtenemos y mostramos mostramos la fecha y hora
+         *      * LocalDateTime.now() obtiene la fecha y hora actuales.
+         *      * DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm") creamos un formato para la fecha y hora según el patrón que le pasemos.
+         *      * fechaHora.format(formatter) convertirá la fecha y hora actual al formato que hemos creado anteriormente.
+         */
         LocalDateTime fechaHora = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
         System.out.println("Fecha de inicio: " + fechaHora.format(formatter));
         System.out.println("-");
 
+        /**
+         * Convertimos el argumento que ha entrado al ejecutar Triangulo a un entero para definir el número total de filas que tendrá el triangulo.
+         */
         int filas = Integer.parseInt(args[0]);
+
+        /**
+         * Generamos el triangulo númerico con el for loop.
+         *      * El primer bucle controla las filas del triangulo yendo de más a menos, cada vez que se ejecuta resta 1 al total de filas.
+         *      * El sergundo bucle imprime cada uno de los numeros que correspondan, en la primera interacción irá del 1 al entero definido por el total de filas.
+         *
+         */
         for (int i=filas; i>=1; i--)
         {
             for (int n=1; n<=i; n++)
--- src/TrianguloLauncher.java
+++ src/TrianguloLauncher.java
@@ -1,8 +1,30 @@
 import java.io.File;
 
+/**
+ * Definición de la clase triangulo.
+ */
 public class TrianguloLauncher {
+
+    /**
+     * Definición del método main. Este método es el que se ejecuta cada vez que se inicia el programa.
+     * Cada una de la partes de la definición del método esta explicado en el fichero de Triangulo.java
+     * @param args
+     */
     public static void main(String[] args) {
+
+        /**
+         * El bloque de try catch para manejar la excepciones que puedan surgir al ejecutar el programa
+         */
         try {
+
+            /**
+             * Creamos un nuevo objeto de clase ProcessBuilder para cada uno de los casos.
+             *      * La clase ProcessBuilder inicia y gestiona un proceso externo desde la aplicación de Java en la que se esta iniciando.
+             *      * La clase ProcessBuilder necesita al menos un argumento, en este caso utilizamos dos el primero es "Java" indica que ejecutaremos un comando con JVM.
+             *      el segundo es la ruta del archivo de java que queremos utilizar. El tercer argumento corresponde con el valor que espera la clase Tringulo para ser ejecutado.
+             *
+             *      * pt1.redirectOutput(new File("triangulo5.txt")) redirecciona la salida de los valores que se mostrarían en consola para que se escriban en el archivo definido.
+             */
             ProcessBuilder pt1 = new ProcessBuilder("java", "src/Triangulo.java", "5");
             pt1.redirectOutput(new File("triangulo5.txt"));
 
@@ -12,6 +34,10 @@ public class TrianguloLauncher {
             ProcessBuilder pt3 = new ProcessBuilder("java", "src/Triangulo.java", "9");
             pt3.redirectOutput(new File("triangulo9.txt"));
 
+            /**
+             *  .start() ejecuta el proceso creado con ProcessBuilder.
+             *  .waitFor() obliga al programa a esperar hasta que termine el proceso.
+             */
             pt1.start().waitFor();
             pt2.start().waitFor();
             pt3.start().waitFor();
